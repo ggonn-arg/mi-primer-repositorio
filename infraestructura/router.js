df9834937b603a53369ab23e680eb42421a8bb0a
gon <gonzaamorales36@gmail.com>
2026-05-06 01:30:34 -0300

    dsadsad

--- infraestructura/router.js
+++
@@ -1,6 +0,0 @@
-import { configureUserRouter } from "./infraestructura/user_router.js";
-
-export function configureRouter(router) {
-    console.log('Configurando router');
-    configureUserRouter(router);
-}
