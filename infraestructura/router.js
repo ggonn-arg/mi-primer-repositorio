026979536205c53f6c863f824568603b61aaee3c
Gonzalo <gonzaamorales36@gmail.com>
2026-05-06 00:30:08 -0300

    gon

--- infraestructura/router.js
+++ infraestructura/router.js
@@ -1,4 +1,4 @@
-import { configureUserRouter } from "./user_router.js";
+import { configureUserRouter } from "./infraestructura/user_router.js";
 
 export function configureRouter(router) {
     console.log('Configurando router');
